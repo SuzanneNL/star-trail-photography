aef14486bf5655e85951cad49f27015a786c9bb7
SuzanneNL <skr470@gmail.com>
2020-11-16 15:56:43 +0000

    Create datepicker

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,11 @@
   $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        maxDate: new Date(),
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
   });
