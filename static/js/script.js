b500847c242e60763f969e4815ffecaf5e909639
SuzanneNL <skr470@gmail.com>
2020-12-24 15:30:45 +0000

    Add comments to code. Add missing ()

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,27 @@
-var currentYear = (new Date).getFullYear();
+//Global variables
+var currentYear = (new Date()).getFullYear();
 
+//Makes the functions inside available after the document is loaded
 $(document).ready(function(){
+
+//Initializes sidenav on the right, for smaller screens
 $('.sidenav').sidenav({edge: "right"});
+
+//Initializes modals
 $('.modal').modal();
+
+//Initializes select option
 $('select').formSelect();
+
+//Initializes Scrollspy, with an offset of 200px
 $('.scrollspy').scrollSpy({
-    scrollOffset: 0
+    scrollOffset: 200
     });
-$("#year").text( (new Date).getFullYear() );
+
+/*Initializes Datepicker. 
+It prevents a user from selecting a date in the future, 
+and navigating to a future year in the dropdown menu*/
+$("#year").text( (new Date()).getFullYear() );
 $('.datepicker').datepicker({
     format: "yyyy/mm/dd",
     changeYear: true,
