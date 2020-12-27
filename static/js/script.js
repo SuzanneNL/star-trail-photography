12910c2899af3cb0e16ee6970a2311ba9ed8c8d8
SuzanneNL <skr470@gmail.com>
2020-12-27 16:54:14 +0000

    Validate and beautify css and js code

--- static/js/script.js
+++ static/js/script.js
@@ -2,34 +2,36 @@
 var currentYear = (new Date()).getFullYear();
 
 //Makes the functions inside available after the document is loaded
-$(document).ready(function(){
+$(document).ready(function() {
 
-//Initializes sidenav on the right, for smaller screens
-$('.sidenav').sidenav({edge: "right"});
-
-//Initializes modals
-$('.modal').modal();
-
-//Initializes select option
-$('select').formSelect();
-
-//Initializes Scrollspy, with an offset of 200px
-$('.scrollspy').scrollSpy({
-    scrollOffset: 50
+	//Initializes sidenav on the right, for smaller screens
+	$('.sidenav').sidenav({
+		edge: "right"
     });
-
-/*Initializes Datepicker. 
-It prevents a user from selecting a date in the future, 
-and navigating to a future year in the dropdown menu*/
-$("#year").text( (new Date()).getFullYear() );
-$('.datepicker').datepicker({
-    format: "yyyy/mm/dd",
-    changeYear: true,
-    maxYear: currentYear,
-    maxDate: new Date(),
-    showClearBtn: true,
-    i18n: {
-        done: "Select"
-    }
-});
+    
+	//Initializes modals
+    $('.modal').modal();
+    
+	//Initializes select option
+    $('select').formSelect();
+    
+	//Initializes Scrollspy, with an offset of 50px
+	$('.scrollspy').scrollSpy({
+		scrollOffset: 50
+    });
+    
+	/*Initializes Datepicker. 
+	It prevents a user from selecting a date in the future, 
+	and navigating to a future year in the dropdown menu*/
+	$("#year").text((new Date()).getFullYear());
+	$('.datepicker').datepicker({
+		format: "yyyy/mm/dd",
+		changeYear: true,
+		maxYear: currentYear,
+		maxDate: new Date(),
+		showClearBtn: true,
+		i18n: {
+			done: "Select"
+		}
+	});
 });
