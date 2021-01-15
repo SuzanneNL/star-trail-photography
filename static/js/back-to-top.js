fab8219db3e685eef46e36373e1c75b4d2eacddd
SuzanneNL <skr470@gmail.com>
2021-01-15 20:46:22 +0000

    Validate and beautify js

--- static/js/back-to-top.js
+++ static/js/back-to-top.js
@@ -4,19 +4,19 @@ var backToTopButton = document.getElementById("backtotop");
 
 // When a user scrolls down 20px from the top of the page, the button appears.
 window.onscroll = function() {
-    scrollFunction();
+	scrollFunction();
 };
 
 function scrollFunction() {
-    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        backToTopButton.style.display = "block";
-    } else {
-        backToTopButton.style.display = "none";
-    }
+	if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		backToTopButton.style.display = "block";
+	} else {
+		backToTopButton.style.display = "none";
+	}
 }
 
 // By clicking on the button, the user scrolls up to the top of the page at once.
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
--- static/js/email.js
+++ static/js/email.js
@@ -1,15 +1,18 @@
+/*Function sendMail connects contact form to the star-trail-photography template from EmailJS
+After submitting the form and a short time out, a success flash message becomes visible.*/
 function sendMail(contactForm) {
-    emailjs.send("service_8n27bcn", "star-trail-photography", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "message": contactForm.message.value
-    })
-    setTimeout(() => {
+	emailjs.send("service_8n27bcn", "star-trail-photography", {
+		"from_name": contactForm.name.value,
+		"from_email": contactForm.emailaddress.value,
+		"message": contactForm.message.value
+	});
+	setTimeout(() => {
 		messageSent();
 	}, 600);
-    return false;  // To block from loading a new page
+	return false; // To block from loading a new page
 }
 
-function messageSent(){
-    document.querySelector('#after-submit').classList.remove('hide');
+//Shows success flash message
+function messageSent() {
+	document.querySelector('#after-submit').classList.remove('hide');
 }
