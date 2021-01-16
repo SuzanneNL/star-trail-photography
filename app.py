eba274ca5a3bdd8d69e87142eb6aa090431c3bc5
SuzanneNL <skr470@gmail.com>
2021-01-16 19:23:16 +0000

    Set debug to false

--- app.py
+++ app.py
@@ -317,4 +317,4 @@ def contact():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
