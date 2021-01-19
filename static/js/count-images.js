866f959407de17b4ff8840ce5a90e0dc24ac5d27
SuzanneNL <skr470@gmail.com>
2021-01-19 20:46:50 +0000

    Add missing semicolon and add comment

--- static/js/count-images.js
+++ static/js/count-images.js
@@ -1,5 +1,7 @@
+/*This script applies to the profile page. There, the number of images on the page is counted.
+If the number is zero, a message becomes visible to the user (that there are no images)*/
 if (document.images.length == 0) {
-    showNoImagesMessage()
+    showNoImagesMessage();
 }
 
 function showNoImagesMessage() {
