411cef9fca785eaaa034eb1ee33e7778147d9e52
SuzanneNL <skr470@gmail.com>
2021-01-20 16:47:31 +0000

    Beautify javascript

--- static/js/initialize-materialize.js
+++ static/js/initialize-materialize.js
@@ -4,39 +4,39 @@ var currentYear = (new Date()).getFullYear();
 //Makes the functions inside available after the document is loaded
 $(document).ready(function() {
 
-    //Initializes sidenav on the right, for smaller screens
-    $('.sidenav').sidenav({
-        edge: "right"
+	//Initializes sidenav on the right, for smaller screens
+	$('.sidenav').sidenav({
+		edge: "right"
     });
-
-    //Initializes modals
+    
+	//Initializes modals
     $('.modal').modal();
-
-    //Initializes select option
+    
+	//Initializes select option
     $('select').formSelect();
-
-    //Initializes Scrollspy, with an offset of 50px
-    $('.scrollspy').scrollSpy({
-        scrollOffset: 50
+    
+	//Initializes Scrollspy, with an offset of 50px
+	$('.scrollspy').scrollSpy({
+		scrollOffset: 50
     });
-
-    //Initializes Character Counter for text areas
+    
+	//Initializes Character Counter for text areas
     $('textarea#message, textarea#description').characterCounter();
-
-    /*Initializes Datepicker. 
-    It prevents a user from selecting a date in the future, 
-    and navigating to a future year in the dropdown menu.
-    Sources: Stack Overflow and eppand.com, see README file
-    under 'Sources'*/
-    $("#year").text((new Date()).getFullYear());
-    $('.datepicker').datepicker({
-        format: "yyyy/mm/dd",
-        changeYear: true,
-        maxYear: currentYear,
-        maxDate: new Date(),
-        showClearBtn: true,
-        i18n: {
-            done: "Select"
-        }
-    });
+    
+	/*Initializes Datepicker. 
+	It prevents a user from selecting a date in the future, 
+	and navigating to a future year in the dropdown menu.
+	Sources: Stack Overflow and eppand.com, see README file
+	under 'Sources'*/
+	$("#year").text((new Date()).getFullYear());
+	$('.datepicker').datepicker({
+		format: "yyyy/mm/dd",
+		changeYear: true,
+		maxYear: currentYear,
+		maxDate: new Date(),
+		showClearBtn: true,
+		i18n: {
+			done: "Select"
+		}
+	});
 });
--- static/js/navbar-active.js
+++ static/js/navbar-active.js
@@ -5,18 +5,19 @@ let urlSplit = url.split('/');
 let thisPage = urlSplit.slice(-1);
 
 function makeActive() {
-    if (thisPage == "") {
-        document.querySelector('#home').classList.add('active');
-    } else if (thisPage == "get_images") {
-        document.querySelector('#gallery').classList.add('active');
-    } else if (thisPage == "contact") {
-        document.querySelector('#contact').classList.add('active');
-    } else if (thisPage == "log_in") {
-        document.querySelector('#login').classList.add('active');
-    } else if (thisPage == "profile_page") {
-        document.querySelector('#profile').classList.add('active');
-    } else if (thisPage == "sign_up") {
-        document.querySelector('#signup').classList.add('active-signup');}
+	if(thisPage == "") {
+		document.querySelector('#home').classList.add('active');
+	} else if(thisPage == "get_images") {
+		document.querySelector('#gallery').classList.add('active');
+	} else if(thisPage == "contact") {
+		document.querySelector('#contact').classList.add('active');
+	} else if(thisPage == "log_in") {
+		document.querySelector('#login').classList.add('active');
+	} else if(thisPage == "profile_page") {
+		document.querySelector('#profile').classList.add('active');
+	} else if(thisPage == "sign_up") {
+		document.querySelector('#signup').classList.add('active-signup');
+	}
 }
 
 makeActive();
